431f6de311afd1f6d61493efea0425d21d987514
Yan Uehara <yanuehara@hotmail.com>
2016-07-22 19:14:27 -0400

    Basic Main() skeleton added Changed project parameters

--- P1_pratica.cpp
+++ P1_pratica.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
+using namespace std;
 
-int main()
-{
+int main(int argc, char* argv[])
+{	
+	if (argc < 2) {
+		cout << "ERRO: Numero de argumentos insuficientes!" << endl;
+		cout << "Uso: jr.exe <arquivo JSON>" << endl;
+
+		return -1;
+	}
+
+	FILE *json;
+	errno_t error;
+	error = fopen_s(&json, argv[1], "r");
+
+	if (json == nullptr ) {
+		cout << "ERRO: Erro na abertura do arquivo, verifique o nome e o caminho" << endl;
+
+		return -1;
+	}
+
     return 0;
 }
 
