f03e037dd7a80d2694f5e487aa1d88e77a08cf9f
Yan Uehara <yanuehara@hotmail.com>
2016-07-22 22:05:46 -0400

    Added code to read the whole file to a char* buffer

--- P1_pratica.cpp
+++ P1_pratica.cpp
@@ -18,13 +18,27 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	ifstream json(argv[1]);
+	ifstream json(argv[1], ios::ios_base::in | ios::ios_base::binary);
 
 	if (json.fail()) {
 		cout << "ERRO: Erro na abertura do arquivo, verifique o nome e o caminho" << endl;
 
 		return -1;
 	}
+
+	Parser json_parser;
+
+	int file_length;
+	json.seekg(0, ios::end);    // go to the end
+	file_length = json.tellg();           // report location (this is the length)
+	json.seekg(0, ios::beg);    // go back to the beginning
+	char *buffer = new char[file_length];    // allocate memory for a buffer of appropriate dimension
+	memset(buffer, 0, file_length);
+	json.read(buffer, file_length);       // read the whole file into the buffer
+	json.close();
+	buffer[file_length] = '\0';
+
+	json_parser.parse(buffer);
 
     return 0;
 }
