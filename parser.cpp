013d016a2af582aee05d116211b02a32993a5627
Yan Uehara <yanuehara@hotmail.com>
2016-07-23 00:17:11 -0400

    Minor corrections Implementation of part of nextToken()

--- parser.cpp
+++ parser.cpp
@@ -6,9 +6,12 @@ void Parser::parse(const char* input) {
 	buffer = const_cast<char*>(input);
 	lineNumber = 1;
 	lookahead = nextToken();
+
+	cout << "Começando o parsing..." << endl;
+
 	Json();
 
-	if (lookahead.type != Token::EOS)
+	if (lookahead.type != Token::_EOF)
 		error();
 
 	cout << "Parsing completo sem erros!" << endl;
@@ -30,19 +33,17 @@ void Parser::Json() {
 
 void Parser::Valor() {
 	//'{', '[', string, numero, TRUE, FALSE, null }
-	switch (lookahead.type) {
-	case Token::ABRECHAVE:
+	if(lookahead.type == Token::ABRECHAVE)
 		Objeto();
-		break;
-	case Token::ABRECOLCHETE:
+	else if(lookahead.type == Token::ABRECOLCHETE)
 		Array();
-		break;
-	case Token::STRING:
-	case Token::NUMERO:
-	case Token::_TRUE:
-	case Token::_FALSE:
-	case Token::_NULL:
-		break;
+	if (lookahead.type == Token::STRING ||
+		lookahead.type == Token::NUMERO ||
+		lookahead.type == Token::_TRUE ||
+		lookahead.type == Token::_FALSE ||
+		lookahead.type == Token::_NULL
+		) {
+		advance();
 	}
 }
 
@@ -79,7 +80,7 @@ void Parser::Par() {
 }
 
 void Parser::P_() {
-	if (lookahead.type = Token::VIRGULA) {
+	if (lookahead.type == Token::VIRGULA) {
 		match(Token::VIRGULA);
 
 		Par();
@@ -123,7 +124,7 @@ void Parser::Elementos() {
 }
 
 void Parser::E_() {
-	if (lookahead.type = Token::VIRGULA) {
+	if (lookahead.type == Token::VIRGULA) {
 		match(Token::VIRGULA);
 		
 		Valor();
@@ -131,4 +132,115 @@ void Parser::E_() {
 	}
 	else
 		;
+}
+
+Token Parser::nextToken() {
+	//"Come" os espaços em branco
+	while (isspace(*buffer))
+		if (*buffer++ == '\n')
+			++lineNumber;
+
+	Token t;
+
+	//Fim da string de parsing
+	if (*buffer == '\0') {
+		t.type = Token::_EOF;
+		return t;
+	}
+
+	//Marca o começo de uma lexeme
+	char* beginLexeme = buffer;
+	
+	//Teste se o token é true
+	if (*buffer == 't') {
+		for (int i = 0; i < 4; i++)
+			++buffer;
+		t.type = Token::_TRUE;
+		t.lexeme = string(beginLexeme, buffer);
+		
+		if (t.lexeme != "true")
+			error();
+
+		return t;
+	}else if (*buffer == 'f') { //Testa se o token é false
+		for (int i = 0; i < 5; i++)
+			++buffer;
+		t.type = Token::_FALSE;
+		t.lexeme = string(beginLexeme, buffer);
+
+		if (t.lexeme != "false")
+			error();
+
+		
+		return t;
+	}else if (*buffer == 'n') { //Testa se o token é null
+		for (int i = 0; i < 4; i++)
+			++buffer;
+		t.type = Token::_NULL;
+		t.lexeme = string(beginLexeme, buffer);
+
+		if (t.lexeme != "null")
+			error();
+
+		return t;
+	}else if (isdigit(*buffer)) {
+		while (isdigit(*buffer))
+			++buffer;
+
+		if (*buffer == '.') { //Tem parte fracionária
+			++buffer;
+			
+			if (!isdigit(*buffer)) error();//Tem que ter pelo menor um numero depois do ponto
+			
+			while (isdigit(*buffer))
+				++buffer;
+		}
+
+		if (*buffer == 'e' || *buffer == 'E') {//Tem parte exponencial
+			++buffer;
+
+			if (*buffer == '+' | *buffer == '-')
+				++buffer;
+			
+			if (!isdigit(*buffer)) error();//Tem que ter pelo menor um numero depois do sinal/expoente
+
+			while (isdigit(*buffer))
+				++buffer;
+		}
+
+		t.type = Token::NUMERO;
+		t.lexeme= string(beginLexeme, buffer);
+
+		return t;
+	}
+	
+	char c = *buffer++;
+	switch (c) {
+		case '{':
+			t.type = Token::ABRECHAVE;
+			t.lexeme = string(beginLexeme, buffer);
+			break;
+		case '}':
+			t.type = Token::FECHACHAVE;
+			t.lexeme = string(beginLexeme, buffer);
+			break;
+		case '[':
+			t.type = Token::ABRECOLCHETE;
+			t.lexeme = string(beginLexeme, buffer);
+			break;
+		case ']':
+			t.type = Token::FECHACOLCHETE;
+			t.lexeme = string(beginLexeme, buffer);
+			break;
+		case ',':
+			t.type = Token::VIRGULA;
+			t.lexeme = string(beginLexeme, buffer);
+			break;
+		case ':':
+			t.type = Token::DOISPONTOS;
+			t.lexeme = string(beginLexeme, buffer);
+			break;
+	}
+
+	return t;
 }
