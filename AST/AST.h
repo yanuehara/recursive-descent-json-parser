73d64613e31c957910871c438686c20f398569b4
Yan Uehara <yanuehara@hotmail.com>
2016-09-17 18:22:03 -0400

    Added main header file of AST

---
+++ AST/AST.h
@@ -0,0 +1,13 @@
+//All the AST includes
+
+#include "Node.h"
+#include "ValueNode.h"
+#include "Value.h"
+#include "Json.h"
+#include "True.h"
+#include "False.h"
+#include "Null.h"
+#include "String.h"
+#include "Par.h"
+#include "Object.h"
+#include "Array.h"
