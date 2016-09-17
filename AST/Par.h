49ca38ac042c054f72b2d794acecebf5f66c210d
Yan Uehara <yanuehara@hotmail.com>
2016-09-17 17:48:32 -0400

    Added definition of AST::Par

---
+++ AST/Par.h
@@ -0,0 +1,36 @@
+#include <string>
+#include "Node.h"
+#include "String.h"
+
+using namespace std;
+
+namespace AST {
+	class Par : public Node {
+		private:
+			String* chave ;
+			ValueNodePtr valor;
+		public:
+			Par() {
+				//Default constructor
+			}
+			
+			Par(String* chave, ValueNodePtr valor) {
+				this->chave = chave;
+				this->valor = valor;
+			}
+			
+			void setChave(String* chave) {
+				this->chave = chave;
+			}
+
+			void setValor(ValueNodePtr valor) {
+				this->valor = valor;
+			}
+
+			void write(Writer& write) const {
+				chave->write(write);
+				write.write(':');
+				valor->write(write);
+			}
+	};
+}
