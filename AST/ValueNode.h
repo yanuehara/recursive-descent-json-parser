1f379ef8c1c51929ddc0841da99952e934c84c6e
Yan Uehara <yanuehara@hotmail.com>
2016-09-17 16:48:36 -0400

    Added definition of AST::ValueNode

---
+++ AST/ValueNode.h
@@ -0,0 +1,22 @@
+#include <string>
+#include "Node.h"
+#include "..\utils\Writer.h"
+
+using namespace std;
+
+namespace AST {
+	class ValueNode : public Node {
+		private:
+			string lexeme;
+		public:
+			ValueNode(string lexeme) {
+				this->lexeme = lexeme;
+			}
+
+			void write(Writer& write) const {
+				write.write(L"%s", lexeme);
+			}
+	};
+	typedef ValueNode* ValueNodePtr;
+
+}
