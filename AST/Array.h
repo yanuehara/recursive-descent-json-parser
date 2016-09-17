7ee1291e843f7ffecb7e5a0ae643d17f8fbf2c6f
Yan Uehara <yanuehara@hotmail.com>
2016-09-17 18:18:44 -0400

    Added definition of AST::Array

---
+++ AST/Array.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+#include "ValueNode.h"
+
+namespace AST {
+	class Array : public ValueNode {
+		private:
+			list<ValueNode> elementos;
+		public:
+			Array() {
+				//Default constructor
+			}
+
+			void insert(ValueNode& elemento) {
+				elementos.push_front(elemento);
+			}
+
+			void write(Writer& write) const {
+				write.write('[');
+				write.tab();
+				
+				for (list<ValueNode>::const_iterator iterator = elementos.begin(); iterator != elementos.end(); iterator++) {
+					iterator->write(write);
+
+					if (elementos.size() == 1) {
+						break;
+					}
+
+					write.write(L"%s", ',\n');
+				}
+
+				write.backspace();
+				write.write(']');
+			}
+	};
+}
